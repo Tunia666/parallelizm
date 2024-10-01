0d4e4718547e85bfed9f14b5500ac02dda49e677
truda <truda1999@gmail.com>
2024-10-01 09:28:47 +0300

    Добавьте файлы проекта.

---
+++ parallelizm/parallelizm.cpp
@@ -0,0 +1,67 @@
+﻿#include <stdio.h>
+#include <omp.h>
+int main()
+{
+    printf("Hello, World!\n");
+
+#pragma omp parallel
+    { int i, n;
+    i = omp_get_thread_num();//	Возвращает число потоков потока, выполняемого в команде потоков.
+    n = omp_get_num_threads();//Возвращает количество потоков в параллельном регионе.
+    printf("I’m thread % d of % d\n", i, n);
+    }
+    return 0;
+}
+
+/*
+#include <stdio.h>
+#include <omp.h>
+int main()
+{
+    int i;
+#pragma omp parallel
+    {
+#pragma omp for
+        for (i = 0; i < 1000; i++)
+            printf(" % d ", i);
+    }
+    return 0;
+}
+*/
+/*
+#include <stdio.h>
+#include <omp.h>
+int main()
+{
+    int i;
+#pragma omp parallel sections private(i)
+    {
+#pragma omp section
+        printf("1st half\n");
+        for (i = 0; i < 500; i++) printf("%d ",i);
+#pragma omp section
+        printf("2nd half\n");
+        for (i = 501; i < 500; i++) printf("%d", i);
+    }
+    return 0;
+}
+*/
+/*
+#include <stdio.h>
+#include <omp.h>
+int main()
+{
+    int i;
+#pragma omp parallel private(i)
+    {
+#pragma omp for
+        for (i = 0; i < 100; i++) printf("% d", i);
+#pragma omp master
+        printf("I’m Master!\n");
+#pragma omp for
+        for (i = 0; i < 100; i++) printf("% d", i);
+    }
+    return 0;
+}
+*/
+
